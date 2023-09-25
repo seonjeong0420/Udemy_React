989bafc20b0d908b6b9b71e14aa69b651e3a5d7a
정선정_WP팀 <sunjeong0420@sk.com>
2023-09-25 15:49:34 +0900

    Initialized React Setup

---
+++ 01-starting-setup/src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import ExpenseDate from './ExpenseDate';
+import './ExpenseItem.css';
+import Card from '../UI/Card';
+
+function ExpenseItem(props) {
+
+  return (
+    <Card className="expense-item">
+      
+      <ExpenseDate date={props.date} />
+      {/* <div>{props.date.toISOString()}</div> */}
+
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <p className="expense-item__price">${props.amount}</p>
+      </div>
+    </Card>
+
+    // <div className="expense-item">
+    //   <div>{props.expense.date.toISOString()}</div>
+    //   <div className="expense-item__description">
+    //     <h2>{props.expense.title}</h2>
+    //     <p className="expense-item__price">${props.expense.amount}</p>
+    //   </div>
+    // </div>
+  )
+}
+
+
+// 방법2) 구조분해할당으로 데이터 props 전달 받는 방식
+// function ExpenseItem({date, title, amount}) {
+//   return (
+
+//     <div className="expense-item">
+//       <div>{date.toISOString()}</div>
+//       <div className="expense-item__description">
+//         <h2>{title}</h2>
+//         <p className="expense-item__price">${amount}</p>
+//       </div>
+//     </div>
+//   )
+// }
+
+export default ExpenseItem
